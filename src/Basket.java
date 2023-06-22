f2cadc271f010b47601ca87ae0eab2ac0b41314e
smac4343 <smac4343@gmail.com>
2023-06-23 00:57:36 +0300

    Add app

---
+++ src/Basket.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Basket implements Serializable {
+    private String[] products;
+    private int[] prices;
+    private int[] quantities;
+    private int totalCost;
+
+    public Basket(String[] products, int[] prices) {
+        this.products = products;
+        this.prices = prices;
+        this.quantities = new int[products.length];
+        this.totalCost = 0;
+    }
+
+    public void addToCart(int productNum, int amount) {
+        quantities[productNum - 1] += amount;
+        totalCost += prices[productNum - 1] * amount;
+    }
+
+    public void printCart() {
+        System.out.println("Your shopping basket:");
+        for (int i = 0; i < products.length; i++) {
+            if (quantities[i] > 0) {
+                System.out.println(products[i] + " x " + quantities[i]);
+            }
+        }
+        System.out.println("Total cost: " + totalCost);
+    }
+
+    public void saveTxt(File textFile) {
+        try (PrintWriter writer = new PrintWriter(textFile)) {
+            for (int i = 0; i < products.length; i++) {
+                writer.println(products[i] + "," + quantities[i]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Basket loadFromTxtFile(File textFile) {
+        try (Scanner scanner = new Scanner(textFile)) {
+            String[] products = new String[5];
+            int[] quantities = new int[5];
+            int[] prices = {50, 30, 60, 10, 100};
+            int totalCost = 0;
+
+            int index = 0;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(",");
+                products[index] = parts[0];
+                quantities[index] = Integer.parseInt(parts[1]);
+                totalCost += prices[index] * quantities[index];
+                index++;
+            }
+
+            Basket basket = new Basket(products, prices);
+            basket.setQuantities(quantities);
+            basket.setTotalCost(totalCost);
+            return basket;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return new Basket(new String[5], new int[5]);
+        }
+    }
+
+    public String[] getProducts() {
+        return products;
+    }
+
+    public int[] getQuantities() {
+        return quantities;
+    }
+
+    public void setQuantities(int[] quantities) {
+        this.quantities = quantities;
+    }
+
+    public int getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(int totalCost) {
+        this.totalCost = totalCost;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] products = {"Milk", "Bread", "Butter", "Eggs", "Ham"};
+        int[] prices = {50, 30, 60, 10, 100};
+        File textFile = new File("basket.txt");
+        Basket basket = Basket.loadFromTxtFile(textFile);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Available products:");
+            for (int i = 0; i < products.length; i++) {
+                System.out.println((i + 1) + ". " + products[i] + " - " + prices[i] + " per unit");
+            }
+
+            System.out.println("Enter product number and quantity (e.g. 3 2 for 2 units of product #3), or 'end' to finish shopping:");
+            String input = scanner.nextLine();
+
+            if ("end".equals(input)) {
+                basket.saveTxt(textFile);
+                break;
+            }
+
+            String[] parts = input.split(" ");
+            int productNumber = Integer.parseInt(parts[0]);
+            int quantity = Integer.parseInt(parts[1]);
+
+            basket.addToCart(productNumber, quantity);
+        }
+
+        basket.printCart();
+    }
+}
